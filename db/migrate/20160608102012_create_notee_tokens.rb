cee7a418f512d89df57bb23d337f08231a9936c5
takujifunao <takuji.funao@gmail.com>
2016-06-08 19:26:20 +0900

    create .env

--- db/migrate/20160608102012_create_notee_tokens.rb
+++ db/migrate/20160608102012_create_notee_tokens.rb
@@ -2,7 +2,7 @@ class CreateNoteeTokens < ActiveRecord::Migration
   def change
     create_table :notee_tokens do |t|
       t.string :api_key
-      t.string :expires_at
+      t.datetime :expires_at
 
       t.timestamps null: false
     end
