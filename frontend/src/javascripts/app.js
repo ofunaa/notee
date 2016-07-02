bd18eb114c4f5262672719bbbc7d482ba84fe8cf
takujifunao <takuji.funao@gmail.com>
2016-07-02 22:34:06 +0900

    delete secret.yml

--- frontend/src/javascripts/app.js
+++ frontend/src/javascripts/app.js
@@ -4,12 +4,9 @@ import ReactDOM from 'react-dom';
 import NoteeApp from './components/NoteeApp.react';
 
 window.onload = function(){
-
-    let appNode = document.createElement('div');
-    document.body.appendChild(appNode);
-
+    
     ReactDOM.render(
-        <NoteeApp />, appNode
+        <NoteeApp />, document.getElementById('react')
     );
 
 }
