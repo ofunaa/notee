ad84424999f6a135b962bc53a50aeaa59e2bf7ab
takujifunao <takuji.funao@gmail.com>
2016-07-05 00:54:39 +0900

    delete extra code in NoteeApp

--- frontend/src/javascripts/components/NoteeApp.react.js
+++ frontend/src/javascripts/components/NoteeApp.react.js
@@ -2,33 +2,7 @@ import React from 'react'
 import Header from './layout/Header.react'
 import Footer from './layout/Footer.react'
 
-var request = require('superagent');
-var BlogStore = require('../stores/BlogStore');
-
-function getBlogState() {
-    return {
-        preview_items: BlogStore.getPreview()
-    };
-}
-
 var NoteeApp  = React.createClass({
-
-    getInitialState: function() {
-        return {
-            posts: [],
-            preview_items: []
-        };
-    },
-
-    componentDidMount: function() {
-        this._loadPosts();
-        BlogStore.addChangeListener(this._onChange);
-    },
-
-    componentWillUnmount: function() {
-        this._loadPosts();
-        BlogStore.removeChangeListener(this._onChange);
-    },
 
     render: function() {
 
@@ -41,15 +15,6 @@ var NoteeApp  = React.createClass({
         );
     },
 
-    _onChange: function() {
-        this.setState(getBlogState());
-    },
-
-    _loadPosts: function() {
-        request.get('/notee/api/posts', (err, res) => {
-            this.setState({posts: res.body.posts});
-        })
-    }
 });
 
 module.exports = NoteeApp;
