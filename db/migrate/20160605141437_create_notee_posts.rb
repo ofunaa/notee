78b77e6dbfc94514bf2b7e656b887cb1158139e7
takujifunao <takuji.funao@gmail.com>
2016-07-12 16:08:30 +0900

    design login page

--- db/migrate/20160605141437_create_notee_posts.rb
+++ db/migrate/20160605141437_create_notee_posts.rb
@@ -1,20 +1,21 @@
+# This migration comes from notee (originally 20160605141437)
 class CreateNoteePosts < ActiveRecord::Migration
   def change
     create_table :notee_posts do |t|
 
       # notee's base
 
-      t.string  :title,    null: false, default: "no title"
-      t.text    :content,  null: false, default: ""
-      t.string  :slug,     null: false, default: "#{Time.now.strftime("%Y-%H-%M-%S")}"
-      t.integer :status,   null: false, default: 0
+      t.string  :title, default: "no title"
+      t.text    :content
+      t.string  :slug, default: "#{Time.now.strftime("%Y-%H-%M-%S")}"
+      t.integer :status, default: 0
       t.integer :category_id, default: 0
       t.integer :thumbnail_id, default: 0
-      t.datetime :published_at, null: false, default: "#{Time.now.strftime("%Y-%H-%M-%S")}"
+      t.datetime :published_at, default: Time.now
 
       # seo
-      t.string  :seo_keyword, null: false, default: ""
-      t.string  :seo_description, null: false, default: ""
+      t.string  :seo_keyword, default: ""
+      t.string  :seo_description, default: ""
 
 
       # if you have user_id
@@ -24,6 +25,6 @@ class CreateNoteePosts < ActiveRecord::Migration
 
     end
 
-    add_index :notee_posts, [:slug], :unique => true
+    add_index :notee_posts, :slug, :unique => true
   end
 end
