dd0017f2cac7bb28ffa6548c1c6b6c00fba7ad1d
takuji funao <takuji.funao@gmail.com>
2016-08-29 23:11:39 +0900

    update at windows

--- app/models/notee/post.rb
+++ app/models/notee/post.rb
@@ -5,10 +5,14 @@ module Notee
     before_create :set_title
     before_create :set_slug
     before_save :set_published_at
+    before_save :check_role
 
     # relations
     belongs_to :category
     belongs_to :thumbnail, :class_name => Notee::Image, :foreign_key => 'thumbnail_id'
+
+    # accessors
+    attr_accessor :editor_id
 
     private
     def set_title
@@ -24,6 +28,10 @@ module Notee
       if self.status != 0
         self.published_at = Time.now
       end
+    end
+
+    def check_role
+
     end
   end
 end
--- app/models/notee/user.rb
+++ app/models/notee/user.rb
@@ -24,6 +24,7 @@ module Notee
 
 	# accessors
 	attr_accessor :password
+  attr_accessor :editor_id
 
 	# callback
 	before_save :encrypt_password
