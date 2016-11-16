9bd4104cc4d29ac4f60c8b4392bb8448d8a852ac
takujifunao <takuji.funao@gmail.com>
2016-11-17 02:54:38 +0900

    add update_method in trash_controler

--- app/controllers/notee/trashes_controller.rb
+++ app/controllers/notee/trashes_controller.rb
@@ -4,14 +4,18 @@ module Notee
 	class TrashesController < ApplicationController
 		def index
 			trash_model = get_model
-
 			if @trashes = trash_model.where(is_delete: true)
 				render json: { status: 'success', trashes: @trashes }
 			else
 				render json: @trashes.errors, status: 422
 			end
+		end
+
+		def update
 
 		end
+
+		private
 
 		def get_model
 			case params[:model]
