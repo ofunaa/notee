84c6d57bf5949cc72f66d94158238295896f2301
takujifunao <takuji.funao@gmail.com>
2016-11-19 01:48:43 +0900

    make trashbox {create TrashMain component & integrate from TrashSection to TrashMain}

---
+++ frontend/src/javascripts/components/trash/TrashMain.react.js
@@ -0,0 +1,79 @@
+import React, {Component, PropTypes} from 'react';
+
+// notee
+import TrashStore from '../../stores/TrashStore';
+import TrashActions from '../../actions/TrashActions';
+import Constants from '../../constants/NoteeConstants';
+
+// common-parts
+import NoteeTable from '../common/table/NoteeTable.react';
+
+
+export default class TrashMain extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            trash_contents: [],
+            columns: []
+        };
+
+        this.ajaxLoaded = this.ajaxLoaded.bind(this);
+        this.changeSuccessed = this.changeSuccessed.bind(this);
+        this.setColumns = this.setColumns.bind(this);
+    }
+
+    componentWillReceiveProps(props) {
+        TrashStore.loadTrashes(props.model_name, this.ajaxLoaded);
+        this.setColumns(props.model_name);
+    }
+
+    componentDidMount() {
+        TrashStore.addChangeListener(Constants.TRASH_UPDATE, this.changeSuccessed);
+    }
+
+    ajaxLoaded(contents){
+        this.setState({trash_contents: contents});
+    }
+
+    render() {
+        return (
+            <NoteeTable
+                modelName="Trash"
+                columns={this.state.columns}
+                contents={this.state.trash_contents}
+                store={TrashStore}
+                actions={TrashActions}
+                ajaxLoad={this.ajaxLoaded}
+            />
+        );
+    }
+
+    changeSuccessed(){
+        TrashStore.loadTrashes(this.props.model_name, this.ajaxLoaded);
+    }
+
+    setColumns(name){
+
+        switch (name){
+            case "categories":
+                this.setState({columns: ['id', 'name', 'slug', 'parent_id', 'is_private']});
+                break;
+            case "posts":
+                this.setState({columns: ['title', 'category', 'status', 'published_at']});
+                break;
+            case "users":
+                this.setState({columns: ['name', 'email', 'role']});
+                break;
+            case "images":
+                this.setState({columns: ['id']});
+                break;
+            case "comments":
+                this.setState({columns: ['post_title', 'name', 'email', 'content', 'is_hidden']});
+                break;
+        }
+    }
+
+
+};
--- frontend/src/javascripts/components/trash/TrashSection.react.js
+++ frontend/src/javascripts/components/trash/TrashSection.react.js
@@ -2,16 +2,10 @@ import React, {Component, PropTypes} from 'react';
 import { Link } from "react-router";
 
 // notee
-import TrashStore from '../../stores/TrashStore';
-import TrashActions from '../../actions/TrashActions';
-import Constants from '../../constants/NoteeConstants';
-
-// common-parts
-import NoteeTable from '../common/table/NoteeTable.react';
+import TrashMain from './TrashMain.react';
 
 // material-ui
 import RaisedButton from 'material-ui/RaisedButton';
-
 
 export default class TrashSection extends Component {
 
@@ -19,82 +13,39 @@ export default class TrashSection extends Component {
         super(props);
 
         this.state = {
-            trash_contents: [],
-            columns: [],
-            model_name: "posts"
+            model_name: null
         };
 
-        this.ajaxLoaded = this.ajaxLoaded.bind(this);
-        this.changeSuccessed = this.changeSuccessed.bind(this);
-        this.setColumns = this.setColumns.bind(this);
+        this.setModelName = this.setModelName.bind(this);
     }
-
+    
     componentDidMount() {
-    }
-
-    componentWillMount() {
-        if(this.props.params.model){
+        if(this.props.params.model != this.state.model_name){
             this.setState({model_name: this.props.params.model});
+        }else{
+            this.setState({model_name: "posts"});
         }
-        TrashStore.addChangeListener(Constants.TRASH_UPDATE, this.changeSuccessed);
-        this.setColumns();
-        TrashStore.loadTrashes(this.state.model_name, this.ajaxLoaded);
-    }
-
-
-    ajaxLoaded(contents){
-        this.setState({trash_contents: contents});
     }
 
     render() {
         var models = ["posts", "categories", "users", "images", "comments"];
-
+        var click = this.setModelName;
         return (
             <div class="main">
                 <h2>Trashbox: {this.state.model_name}</h2>
                 {models.map((content, index)=>{
                     return (
                         <Link to={`/notee/trashes/${content}`} activeClassName="active" class="mr_20" key={index}>
-                            <RaisedButton label={content} primary={true} />
+                            <RaisedButton onClick={function(){click(content)}} label={content} primary={true} />
                         </Link>
                     );
                 })}
-                <NoteeTable
-                    modelName="Trash"
-                    columns={this.state.columns}
-                    contents={this.state.trash_contents}
-                    store={TrashStore}
-                    actions={TrashActions}
-                    ajaxLoad={this.ajaxLoaded}
-                />
+                <TrashMain model_name={this.state.model_name} />
             </div>
         );
     }
 
-    changeSuccessed(){
-        TrashStore.loadTrashes(this.state.model_name, this.ajaxLoaded);
+    setModelName(name) {
+        this.setState({model_name: name});
     }
-
-    setColumns(){
-
-        switch (this.state.model_name){
-            case "categories":
-                this.setState({columns: ['id', 'name', 'slug', 'parent_id', 'is_private']});
-                break;
-            case "posts":
-                this.setState({columns: ['title', 'category', 'status', 'published_at']});
-                break;
-            case "users":
-                this.setState({columns: ['name', 'email', 'role']});
-                break;
-            case "images":
-                this.setState({columns: ['id']});
-                break;
-            case "comments":
-                this.setState({columns: ['post_title', 'name', 'email', 'content', 'is_hidden']});
-                break;
-        }
-    }
-    
-
 };
