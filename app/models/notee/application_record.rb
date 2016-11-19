a0a33ffe33b217f95aad9b8a016866b539f3f88f
takujifunao <takuji.funao@gmail.com>
2016-11-19 21:51:46 +0900

    add trash & time_limit scope

--- app/models/notee/application_record.rb
+++ app/models/notee/application_record.rb
@@ -2,6 +2,10 @@ module Notee
   class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
 
+    # scopes
+    scope :trash, -> { where(is_delete: true) }
+    scope :time_limit, -> { where('updated_at <= ?', Time.current - 60*60*24*30) }
+
     # authority check
     before_create :create_authority
     before_update :update_authority
--- app/models/notee/comment.rb
+++ app/models/notee/comment.rb
@@ -1,5 +1,11 @@
 module Notee
   class Comment < ActiveRecord::Base
+
+    # scopes
+    scope :trash, -> { where(is_delete: true) }
+    scope :time_limit, -> { where('updated_at <= ?', Time.current - 60*60*24*30) }
+
+    # validates
     validates :post_id, presence: true
     validates :content, presence: true
   end
