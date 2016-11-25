d0a9354ad85ebd1be9b14fc35b1bce3748230d69
takujifunao <takuji.funao@gmail.com>
2016-11-25 13:33:01 +0900

    make RoleStore

---
+++ frontend/src/javascripts/stores/RoleStore.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import assign from 'object-assign';
+import request from 'superagent';
+
+var EventEmitter = require('events').EventEmitter;
+
+
+var RoleStore = assign({}, EventEmitter.prototype, {
+
+    loadRole: function(callback) {
+        var url = "/notee/api/roles/1";
+        request.get(url, (err, res) => {
+            if(err){return;}
+            if(!res.body){return;}
+            callback(res.body.role);
+        })
+    },
+
+    loadRoles: function(callback) {
+        var url = "/notee/api/roles";
+        request.get(url, (err, res) => {
+            if(err){return;}
+            if(!res.body){return;}
+            callback(res.body.roles);
+        })
+    },
+
+    emitChange: function(change_event) {
+        this.emit(change_event);
+    },
+
+    addChangeListener: function(change_event, callback) {
+        this.on(change_event, callback);
+    }
+
+});
+
+module.exports = RoleStore;
