10f8ae2b60f5908901e0aea9cd071a6d6f56b21c
takujifunao <takuji.funao@gmail.com>
2016-11-26 02:07:36 +0900

    make display how can i do

--- frontend/src/javascripts/components/mypage/MypageSection.react.js
+++ frontend/src/javascripts/components/mypage/MypageSection.react.js
@@ -32,6 +32,82 @@ export default class UserShow extends Component {
 
     render() {
 
+        const RoleDescription = function(role){
+
+            var can_do_list = [];
+            var can_does = role_desription("can", role);
+            for(var i in can_does){
+                can_do_list.push(<li key={i}>{can_does[i]}</li>);
+            }
+
+            var can_not_do_list = [];
+            var can_not_does = role_desription("cannot", role);
+            for(var i in can_not_does){
+                can_not_do_list.push(<li key={i}>{can_not_does[i]}</li>);
+            }
+
+            return(
+                <div className="role_description">
+                    <h3>You can</h3>
+                    {can_do_list}
+                    <h3>You can't</h3>
+                    {can_not_do_list}
+                </div>
+            );
+        };
+
+        const role_desription = function(can_or_not, role){
+            if(can_or_not == "can"){
+                switch(role){
+                    case "writer":
+                        return [
+                            "Create: posts, categories, images",
+                            "Update: my posts, categories, images, my user without role",
+                            "Delete: my posts"]
+                    case "editor":
+                        return [
+                            "Create: posts, categories, images",
+                            "Update: posts, categories, images, my user without role",
+                            "Delete: posts, categories, images"]
+                    case "manager":
+                        return [
+                            "Create: posts, categories, images, users",
+                            "Update: posts, categories, images, users",
+                            "Delete: posts, categories, images, users"]
+                    case "root":
+                        return [
+                            "Create: users",
+                            "Update: none",
+                            "Delete: none"]
+                }
+            }else{
+                switch(role){
+                    case "writer":
+                        return [
+                            "Create: users",
+                            "Update: other's posts, other users, my user role",
+                            "Delete: other's posts, categories, images, users"]
+                    case "editor":
+                        return [
+                            "Create: users",
+                            "Update: other users, my user role",
+                            "Delete: users"]
+                    case "manager":
+                        return [
+                            "Create: none",
+                            "Update: none",
+                            "Delete: none"]
+                    case "root":
+                        return [
+                            "Create: posts, categories, images",
+                            "Update: posts, categories, images, users",
+                            "Delete: posts, categories, images, users"]
+                }
+            }
+
+
+        }
+
         return(
             <div>
                 <h2>Mypage</h2>
@@ -42,15 +118,16 @@ export default class UserShow extends Component {
                     <RaisedButton label="Edit Password" primary={true} />
                 </Link>
                 <p>name:</p>
-                <p>{this.state.user.name}</p>
+                <h3>{this.state.user.name}</h3>
                 <p>email:</p>
-                <p>{this.state.user.email}</p>
+                <h3>{this.state.user.email}</h3>
                 <p>profile:</p>
-                <p>{this.state.user.profile}</p>
+                <h3>{this.state.user.profile}</h3>
                 <p>profile_img:</p>
                 <img src={this.state.profile_img} />
                 <p>role:</p>
-                <p>{this.state.user.role}</p>
+                <h3>{this.state.user.role}</h3>
+                {RoleDescription(this.state.user.role)}
             </div>
         );
     }
