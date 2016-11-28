78989e1d90a29a8ee049225a961114fbb14f824d
takujifunao <takuji.funao@gmail.com>
2016-11-28 20:03:57 +0900

    create AuthorityButtonCreate

---
+++ frontend/src/javascripts/components/common/authority/AuthorityButtonCreate.react.js
@@ -0,0 +1,97 @@
+import React, {Component, PropTypes} from 'react';
+
+// material-ui
+import RaisedButton from 'material-ui/RaisedButton';
+
+export default class AuthorityButtonCreate extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    shouldComponentUpdate(){
+        if(!this.props.now_user){return false;}
+        return true;
+    }
+
+    render() {
+
+        var createButton = function(model, now_user, createMethod){
+            switch(model){
+                case "Category":
+                    switch(now_user.role){
+                        case "writer":
+                        case "root":
+                        case "suspended":
+                            return (
+                                <RaisedButton
+                                    label="no permit"
+                                    disabled={true}
+                                />
+                            );
+                        default:
+                            return (
+                                <RaisedButton
+                                    onClick={createMethod}
+                                    label="delete"
+                                    secondary={true}
+                                    disabled={false}
+                                />
+                            );
+                    }
+                case "User":
+                    switch(now_user.role){
+                        case "writer":
+                        case "editor":
+                        case "root":
+                        case "suspended":
+                            return (
+                                <RaisedButton
+                                    label="no permit"
+                                    disabled={true}
+                                />
+                            );
+                        default:
+                            return (
+                                <RaisedButton
+                                    onClick={createMethod}
+                                    label="delete"
+                                    secondary={true}
+                                    disabled={false}
+                                />
+                            );
+                    }
+                case "Image":
+                    switch(now_user.role){
+                        case "writer":
+                        case "root":
+                        case "suspended":
+                            return (
+                                <RaisedButton
+                                    label="no permit"
+                                    disabled={true}
+                                />
+                            );
+                        default:
+                            return (
+                                <RaisedButton
+                                    onClick={createMethod}
+                                    label="delete"
+                                    secondary={true}
+                                    disabled={false}
+                                />
+                            );
+                    }
+            }
+
+        }
+
+        return(
+            <div>
+                {createButton(this.props.modelName, this.props.now_user, this.props.createMethod)}
+            </div>
+        );
+    }
+
+};
+
