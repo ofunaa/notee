87bb21984f9c9da2aeb93e320ad1447d790a17d7
funaota <takuji.funao@gmail.com>
2016-12-05 20:59:44 +0900

    refactoring

--- lib/tasks/controllers/notee_controller.rb
+++ lib/tasks/controllers/notee_controller.rb
@@ -1,11 +1,11 @@
 class NoteeController < ApplicationController
 
   before_action :set_meta_info, except: [:show]
-  before_action :set_title, only: [:index, :category_posts, :archives_posts, :writer_posts]
 
   # GET "/"
   def index
     @posts = notees
+    @notee_title = 'Post Lists'
     render :action => 'posts'
   end
 
@@ -16,60 +16,46 @@ class NoteeController < ApplicationController
     @notee_meta = notee_set_meta_by_post(@post)
   end
 
-  # GET "/category"
+  # GET "/categories"
   def categories
     @categories = notee_categories
   end
 
-  # GET "/category/:name_or_slug"
+  # GET "/categories/:name_or_slug"
   def category_posts
-    redirect_to root_path if params[:name_or_slug].nil?
     @posts = category_notees(params[:name_or_slug])
+    @notee_title = 'Category: ' + params[:name_or_slug]
     render :action => 'posts'
   end
 
-  # GET "/archive"
+  # GET "/archives"
   def archives
     @archives = notee_archives
   end
 
-  # GET "/archive/:year"
-  # GET "/archive/:year/:month"
+  # GET "/archives/:year"
+  # GET "/archives/:year/:month"
   def archive_posts
-    redirect_to root_path if params[:year].nil?
     @posts = archive_notees(params[:year], params[:month].present? ? params[:month] : nil)
+    @notee_title = 'Archive: ' + params[:year] + (params[:month].present? ? "/" + params[:month].to_s : "")
     render :action => 'posts'
   end
 
-  # GET "/writer"
+  # GET "/writers"
   def writers
     @writers = notee_writers
   end
 
-  # GET "/writer/:name_or_id"
+  # GET "/writers/:name_or_id"
   def writer_posts
-    redirect_to root_path if params[:name_or_id].nil?
     @posts = writer_notees(params[:name_or_id])
+    @notee_title = 'Writer: ' + params[:name_or_id]
     render :action => 'posts'
   end
 
   private
   def set_meta_info
     @notee_meta = Notee.blog_meta
-  end
-
-  def set_title
-    case request.fullpath
-      when notee_public_index_path
-        return @notee_title = 'Notee Index'
-      when notee_public_category_posts_path
-        return @notee_title = 'Category: ' + params[:name_or_slug]
-      when notee_public_archive_posts_path
-        return @notee_title = 'Archive: ' + params[:year] + params[:month].present? ? "/" + params[:month].to_s : ""
-      when notee_public_writer_posts_path
-        return @notee_title = 'Writer: ' + params[:name_or_id]
-    end
-    return @notee_title = 'Notee'
   end
 
 end
