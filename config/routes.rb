d0b12d20d16c3be0d861d63dd185bc27bdcdbb86
funaota <takuji.funao@gmail.com>
2016-12-13 02:05:33 +0900

    refactor notee_route

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,6 @@ Notee::Engine.routes.draw do
 
   root to: 'notees#index'
 
-  get 'posts'                 => 'notees#index'
-  get 'posts/new'             => 'notees#index'
-  get 'posts/edit/:id'        => 'notees#index'
-  get 'categories'            => 'notees#index'
-  get 'categories/edit/:id'   => 'notees#index'
-  get 'images'                => 'notees#index'
-  get 'comments'              => 'notees#index'
-  get 'users'                 => 'notees#index'
-  get 'users/new'             => 'notees#index'
-  get 'users/edit/:id'        => 'notees#index'
-  get 'trashes'               => 'notees#index'
-  get 'trashes/:model'        => 'notees#index'
-  get 'mypage'                => 'notees#index'
-  get 'mypage/edit'           => 'notees#index'
-  get 'mypage/edit/password'  => 'notees#index'
-
   # post 'secret_published' => 'notees#secret_published'
   resources :tokens, only: [:new, :create, :destroy]
 
@@ -33,4 +17,6 @@ Notee::Engine.routes.draw do
     resources :roles, only: [:index, :show]
     resources :trashes, only: [:index, :update]
   end
+
+  get '*anything'             => 'notees#index'
 end
