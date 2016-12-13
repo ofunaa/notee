5e493a232067d5e298a3ba22e03b3d5210978cdc
funaota <takuji.funao@gmail.com>
2016-12-14 04:59:39 +0900

    add reCAPTCHA to login form in tokenController

--- app/controllers/notee/tokens_controller.rb
+++ app/controllers/notee/tokens_controller.rb
@@ -1,6 +1,10 @@
 
 require_dependency 'notee/application_controller'
 
+require 'open-uri'
+require 'json'
+require 'uri'
+
 module Notee
   class TokensController < ApplicationController
     skip_before_filter :restrict_access_json, only: [:new, :create]
@@ -9,6 +13,7 @@ module Notee
     end
 
     def create
+      check_recaptcha
       User.sign_in(params[:id], params[:password])
       redirect_to root_path
     end
@@ -21,6 +26,16 @@ module Notee
           else
           format.json { render json: @token.errors, status: :unprocessable_entity }
         end
+      end
+    end
+
+    def check_recaptcha
+      endpoint = 'https://www.google.com/recaptcha/api/siteverify?secret=' + Notee.recaptcha_secret_key + '&response=' + params["g-recaptcha-response"]
+      uri = URI.parse(endpoint)
+      json = Net::HTTP.get(uri)
+      result = JSON.parse(json)
+      if result["success"] == false
+        raise
       end
     end
   end
