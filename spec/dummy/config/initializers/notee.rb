3928bb924e85804d7bfe328da77c7c0c17f81654
funaota <takuji.funao@gmail.com>
2017-01-04 18:18:54 +0900

    delete dummy

--- spec/dummy/config/initializers/notee.rb
+++
@@ -1,8 +0,0 @@
-require 'notee'
-
-# Recommendation using .env for manage id & password
-
-Notee.configure do |config|
-  config.notee_id = "hogehoge"
-  config.notee_password = "hogehoge"
-end
--- spec/dummy/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-
-  mount Notee::Engine => "/notee"
-
-  get '/' => 'blog#index'
-  get '/category/:category_slug' => 'blog#notee_category'
-  get '/:id' => 'blog#notee_id'
-  get '/:slug' => 'blog#notee_slug'
-end
