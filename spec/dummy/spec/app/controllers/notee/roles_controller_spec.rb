a07afcaed4cfab594cd6acbc6166c478af64489b
funaota <takuji.funao@gmail.com>
2017-01-04 21:27:56 +0900

    WIP create roles_controller spec

--- spec/dummy/spec/app/controllers/notee/roles_controller_spec.rb
+++ spec/dummy/spec/app/controllers/notee/roles_controller_spec.rb
@@ -1,10 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Notee::RolesController, type: :controller do
-  describe 'GET #index' do
-    it "render json: {status: success, roles: User.roles}" do
-      get :index
-      expect(response).to be_success
+
+  describe 'public' do
+    routes { Notee::Engine.routes }
+    context 'GET #index' do
+      it "render json: {status: success, roles: User.roles}" do
+        get :index
+        expect(response).to be_success
+      end
+    end
+
+    context 'GET #show' do
+      context ':find_user_by_access_token success' do
+        it "render json: { status: 'success', role: user.role }" do
+          get :show
+          expect(response).to be_success
+        end
+      end
+
+      context ':find_user_by_access_token failed' do
+        it "render json: { status: 'failed' }" do
+          get :show
+          expect(response).to be_success
+        end
+      end
     end
   end
+
+
+  describe 'private' do
+    context ':find_user_by_access_token' do
+      context 'success' do
+        it "render json: { status: 'success', role: user.role }" do
+          user = find_user_by_access_token
+          expect(user).to be_nil
+        end
+      end
+
+      context 'failed' do
+        it "render json: { status: 'failed' }" do
+          user = find_user_by_access_token
+          expect(user).to be_nil
+        end
+      end
+    end
+  end
+
 end
